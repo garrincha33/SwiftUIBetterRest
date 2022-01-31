5305b5e3a620862bdb933b4c564a50fa4cd228f5
Richard Price <richyprice77@gmail.com>
2022-01-31 10:02:05 +0000

    inital commit

---
+++ SwiftUIBetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBetterRest
+//
+//  Created by Richard Price on 31/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World, Hello Everyone!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBetterRest/SwiftUIBetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBetterRestApp.swift
+//  SwiftUIBetterRest
+//
+//  Created by Richard Price on 31/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
